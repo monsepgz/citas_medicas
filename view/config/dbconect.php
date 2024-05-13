14fd5bbf2da4083e3236ace18dc58e3e15a2c1dc
monsepgz <200i0104@tecmartinez.edu.mx>
2024-05-13 17:25:24 -0600

    edicion de la conexion

--- view/config/dbconect.php
+++ view/config/dbconect.php
@@ -2,27 +2,36 @@
 
 Class Connection{
  
-	private $server = "mysql:host=localhost;dbname=proyecto_final";
-	private $username = "root";
-	private $password = "";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
-	protected $conn;
- 	
-	public function open(){
- 		try{
- 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
- 			return $this->conn;
- 		}
- 		catch (PDOException $e){
- 			echo "Hubo un problema con la conexión: " . $e->getMessage();
- 		}
- 
+    // Información de la base de datos
+    private $server = "mysql:host=localhost;dbname=proyecto_final";
+    private $username = "root";
+    private $password = "";
+    private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+    
+    // Variable para almacenar la conexión a la base de datos
+    protected $conn;
+     
+    // Método para abrir la conexión a la base de datos
+    public function open(){
+        try{
+            // Intenta establecer la conexión
+            $this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+            
+            // Si la conexión es exitosa, retorna el objeto PDO
+            return $this->conn;
+        }
+        catch (PDOException $e){
+            // Si hay un error, muestra el mensaje
+            echo "Hubo un problema con la conexión: " . $e->getMessage();
+        }
     }
  
-	public function close(){
-   		$this->conn = null;
- 	}
+    // Método para cerrar la conexión a la base de datos
+    public function close(){
+        // Cierra la conexión asignando null a la variable $conn
+        $this->conn = null;
+    }
  
 }
- 
+
 ?>
